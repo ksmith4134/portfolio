7c42f37e26dc9461cc735823f8f92fafe2601abc
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2023-09-15 23:41:38 -0400

    Deleted unused modules on /holo page

--- src/pages/holo.js
+++ src/pages/holo.js
@@ -1,13 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import ThreeDCard from "@/components/widgets/hologram/3DCard/ThreeDCard";
-import HologramCardLinear from "@/components/widgets/hologram/LinearApp/HologramCardLinear";
-import HologramInitials from "@/components/widgets/hologram/Initials/HologramInitials";
-import HologramTemplate from "@/components/widgets/hologram/HologramTemplate";
 import HologramBitcoin from "@/components/widgets/hologram/Bitcoin/HologramBitcoin";
 import HologramReact from "@/components/widgets/hologram/React/HologramReact";
 import HologramPinkFloyd from "@/components/widgets/hologram/PinkFloyd/HologramPinkFloyd";
-import LineGraph from "@/components/lineGraph";
-import IconRender from "@/components/media/iconRender";
 
 export default function Holo() {
 
